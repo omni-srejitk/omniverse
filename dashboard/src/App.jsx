7cb66f3e3ecfb35a12bba2b135286bf74453c494
Sreejith K <sreejith.k@omniflo.in>
2022-10-31 13:44:11 +0530

    Chore : Minor code changes

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Header } from './components/Header/Header';
+import { Sidebar } from './components/Sidebar/Sidebar';
 import { RouterConfig } from './config/RouterConfig';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App flex'>
+      <Sidebar />
       <Header />
       <RouterConfig />
     </div>
