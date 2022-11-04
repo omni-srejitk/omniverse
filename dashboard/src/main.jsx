5900bbbf8f5a41868a54f22b8464420a0fda0507
Sreejith K <sreejith.k@omniflo.in>
2022-11-04 18:24:49 +0530

    Feat : Added React Query Dependency

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+export const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
         <App />
-      </Provider>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>
 );
