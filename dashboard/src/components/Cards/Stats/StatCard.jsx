1016768ffcb0cd1313097aa761df8e3c755d4c61
Sreejith K <sreejith.k@omniflo.in>
2022-11-07 17:12:26 +0530

    Fix : Added Currency Indicator

--- dashboard/src/components/Cards/Stats/StatCard.jsx
+++ dashboard/src/components/Cards/Stats/StatCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const StatCard = ({ icon, title, metric, color }) => {
+export const StatCard = ({ icon, title, metric, color, currency = false }) => {
   return (
     <div
       className={`mr-5 flex h-full min-w-[20rem] flex-col  items-start justify-start rounded-xl lg:min-w-[calc(33.33%-12px)] ${color} p-8`}
@@ -10,7 +10,11 @@ export const StatCard = ({ icon, title, metric, color }) => {
       </div>
       <div className='mt-6'>
         <p className='text-base font-semibold'>{title}</p>
-        <h3 className=' text-5xl font-semibold'>{metric}</h3>
+        <h3 className=' text-5xl font-semibold'>
+          {' '}
+          {currency && <span>&#8377;</span>}
+          {metric}
+        </h3>
         <div className='stock__indicator my-4 flex -items-center gap-2 rounded-full bg-red-300 p-1'>
           <span className='material-icons'>arrow_upward</span>
           37.8%
