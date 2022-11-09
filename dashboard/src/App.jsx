36134cdb81b86c7dd22e9afd5c9d2ed5f9287d42
Sreejith K <sreejith.k@omniflo.in>
2022-11-09 18:16:05 +0530

    Style : Added Fonts + React-Hot-Toast

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,13 +2,14 @@ import './App.css';
 import { Header } from './components/Header/Header';
 import { Sidebar } from './components/Sidebar/Sidebar';
 import { RouterConfig } from './config/RouterConfig';
-
+import { Toaster } from 'react-hot-toast';
 function App() {
   return (
     <div className='App'>
       <Sidebar />
       <Header />
       <RouterConfig />
+      <Toaster />
     </div>
   );
 }
