739ecd61bea485c57c31d50a79f00991d3637843
Sreejith K <sreejith.k@omniflo.in>
2022-11-09 18:27:20 +0530

    Style : Updated graph styles

--- dashboard/src/components/Charts/Charts.jsx
+++ dashboard/src/components/Charts/Charts.jsx
@@ -7,15 +7,12 @@ import {
   YAxis,
   Tooltip,
   ResponsiveContainer,
+  CartesianGrid,
 } from 'recharts';
-export const Charts = ({
-  data = [],
-  filter = 'All Time',
-  color = '#8884d8',
-}) => {
+export const AreaCharts = ({ data = [], filter = 'All Time', color }) => {
   return (
-    <ResponsiveContainer width='100%' height='100%'>
-      <AreaChart width={1000} height={1000} data={data}>
+    <ResponsiveContainer width={'100%'} height={'80%'}>
+      <AreaChart width={'100%'} height={'100% '} data={data}>
         <defs>
           <linearGradient id='color' x1='0' y1='0' x2='0' y2='1'>
             <stop offset='0%' stopColor={color} stopOpacity='0.9' />
@@ -24,19 +21,31 @@ export const Charts = ({
         </defs>
         <XAxis
           dataKey='Date'
-          tickMargin={1}
+          tickMargin={0}
+          axisLine={false}
+          tickLine={false}
           tickCount={data.length}
+          interval={'preserveStartEnd'}
           tickFormatter={(str) => {
-            return moment(str, 'DD-MM-YY').format('D');
+            return moment(str, 'DD-MM-YY').format('Do');
           }}
         />
-        <YAxis dataKey={'Cumalative'} />
+        <YAxis
+          dataKey='Sales'
+          tickLine={false}
+          axisLine={false}
+          interval={'preserveStartEnd'}
+        />
+        <CartesianGrid opacity={'0.2'} />
         <Tooltip />
         <Area
           type='monotone'
           dataKey={'Sales'}
+          width={'100%'}
+          height={'90%'}
           stroke={color}
           fill='url(#color)'
+          axisLine={false}
         />
       </AreaChart>
     </ResponsiveContainer>
