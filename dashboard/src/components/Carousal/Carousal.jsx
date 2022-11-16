4b7bdfdd285e050e6a8750c1209e960f2453a159
Sreejith K <sreejith.k@omniflo.in>
2022-11-16 16:42:56 +0530

    Fix : Incrementing last image error

--- dashboard/src/components/Carousal/Carousal.jsx
+++ dashboard/src/components/Carousal/Carousal.jsx
@@ -19,17 +19,22 @@ export const Carousal = () => {
   };
 
   useEffect(() => {
-    if (currentIndex > MAX_LENGTH && currentIndex < IMG_DATA?.length - 1) {
+    if (currentIndex > MAX_LENGTH) {
       MAX_LENGTH += IMAGES_LOADED;
-    } else if (
+    }
+    if (
       currentIndex > MAX_LENGTH &&
-      currentIndex === IMG_DATA?.length - 1
+      currentIndex === Object.keys(IMG_DATA)?.length - 1
     ) {
       MAX_LENGTH = IMAGES_LOADED;
       setCurrentIndex(0);
     }
+    if (currentIndex > Object.keys(IMG_DATA)?.length - 1) {
+      setCurrentIndex(0);
+    }
     if (currentIndex < 0) {
-      setCurrentIndex(MAX_LENGTH);
+      const PICS_LENGTH = Object.keys(IMG_DATA)?.length - 1;
+      setCurrentIndex(PICS_LENGTH);
     }
   }, [currentIndex]);
 
@@ -53,7 +58,8 @@ export const Carousal = () => {
               <span className='material-icons m-0 p-0 '>chevron_left</span>
             </button>
             <button
-              className={`flex h-12 w-12 items-center justify-center rounded-full border-2 border-gray-200 hover:bg-gray-300`}
+              disabled={Object.keys(IMG_DATA)?.length - 1 === currentIndex}
+              className={`flex h-12 w-12 items-center justify-center rounded-full border-2 border-gray-200 text-gray-400 disabled:bg-gray-100 disabled:text-gray-300 hover:bg-gray-300 hover:bg-gray-300`}
               onClick={() => handleClick('NEXT')}
             >
               <span className='material-icons m-0 p-0 text-gray-400'>
