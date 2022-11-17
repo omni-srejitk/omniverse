ca05bd28b975250bd4ee6ec3aca7f1f42b67f8ae
Sreejith K <sreejith.k@omniflo.in>
2022-11-17 18:56:41 +0530

    Style : Updated styles

--- dashboard/src/components/ModalGallery/ModalGallery.jsx
+++ dashboard/src/components/ModalGallery/ModalGallery.jsx
@@ -8,6 +8,7 @@ export const ModalGallery = () => {
   const IMAGES_LOADED = 5;
   const BRAND = JSON.parse(localStorage.getItem('Name'));
   const { isLoading: ImageLoading, data: ImageData } = fetchStoreImages(BRAND);
+
   const IMG_DATA = !ImageLoading && ImageData?.data['message'];
 
   const handleClick = (type) => {
@@ -34,48 +35,65 @@ export const ModalGallery = () => {
   }, [currentIndex]);
 
   return (
-    <div className='flex h-full max-h-full flex-col items-center justify-start overflow-hidden rounded-lg pb-0'>
-     
-      <div className='relative flex h-full  w-full flex-grow overflow-hidden rounded-xl'>
+    <div className='relative z-10 flex h-full max-h-full w-full flex-col items-center justify-start overflow-hidden rounded-lg'>
+      <div className='absolute top-1/2 z-40 mb-4 flex w-full -translate-y-1/2 items-center justify-between overflow-hidden lg:static lg:top-0 lg:order-2 lg:h-fit lg:translate-y-0'>
+        {!ImageLoading && Object.keys(IMG_DATA)?.length > 1 ? (
+          <div className='z-40 flex h-14 w-full items-center justify-between gap-4 px-6 lg:mt-6 lg:px-0'>
+            <button
+              className={`z-40 flex h-12 w-12 items-center justify-center rounded-full border-2 border-gray-200 bg-white text-gray-400 disabled:bg-gray-100 disabled:text-gray-400 hover:border-gray-400 hover:bg-gray-100 hover:text-black`}
+              onClick={() => handleClick('PREVIOUS')}
+              disabled={currentIndex === 0 ? true : false}
+            >
+              <span className='material-icons m-0 p-0 '>chevron_left</span>
+            </button>
+            <button
+              disabled={Object.keys(IMG_DATA)?.length - 1 === currentIndex}
+              className={`z-40 flex h-12 w-12 items-center justify-center rounded-full border-2 border-gray-200 bg-white text-gray-400 disabled:bg-gray-100 disabled:text-gray-400 hover:border-gray-400 hover:bg-gray-100 hover:text-black`}
+              onClick={() => handleClick('NEXT')}
+            >
+              <span className='material-icons m-0 p-0 text-gray-400'>
+                chevron_right
+              </span>
+            </button>
+          </div>
+        ) : null}
+      </div>
+      <div className='relative z-30 flex h-full w-full flex-grow overflow-hidden rounded-xl'>
         {ImageLoading ? (
           <Spinner
             color={'border-blue-200'}
-            position={'top-1/2 left-1/2'}
+            position={'top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'}
             loading={ImageLoading}
           />
         ) : (
           <div
-            className={`left-0 top-0 bottom-0 flex h-full w-full flex-grow items-start justify-center gap-4 overflow-hidden `}
+            className={`flex h-full w-full flex-grow items-center justify-center gap-4 overflow-hidden lg:left-0 lg:top-0 lg:bottom-0`}
           >
-            <img
-              className='z-10  h-full w-full rounded-xl object-cover'
-              src={
-                !ImageLoading &&
-                IMG_DATA[currentIndex]['status'] &&
-                IMG_DATA[currentIndex]['image']
-              }
-            />
+            {!ImageLoading && Object.keys(IMG_DATA)?.length > 0 ? (
+              <img
+                className='z-10 h-full w-full rounded-xl object-cover'
+                src={
+                  !ImageLoading &&
+                  IMG_DATA[currentIndex]['status'] &&
+                  IMG_DATA[currentIndex]['image']
+                }
+              />
+            ) : (
+              <div className='flex h-full w-full items-center justify-center'>
+                <div className='flex flex-col items-center justify-center gap-4'>
+                  <span className='material-icons flex h-20 w-20 items-center justify-center rounded-full bg-gray-200 text-4xl text-gray-400'>
+                    camera_enhance
+                  </span>
+                  <p className=' text-center text-xl'>Come back soon.</p>
+                  <p className=' text-center font-semibold text-gray-500'>
+                    We're currently curating pictures for your products.
+                  </p>
+                </div>
+              </div>
+            )}
           </div>
         )}
       </div>
-      <div className='absolute mb-4 flex w-full items-center justify-between top-[92%] left-[4%] z-1500'>
-        <div className='flex h-8 w-fit items-center justify-between gap-[11rem] z-1500'>
-          <button
-            className={`flex h-8 w-8 items-center justify-center border-gray-200 text-black disabled:bg-white disabled:text-black  hover:bg-gray-300`}
-            onClick={() => handleClick('PREVIOUS')}
-            disabled={currentIndex === 0 ? true : false}
-          >
-            <span className='material-icons m-0 p-0 '>arrow_back</span>
-          </button>
-          <button
-            className={`flex h-8 w-8 items-center justify-center border-gray-200 text-black disabled:bg-white disabled:text-black hover:bg-gray-300`}
-            onClick={() => handleClick('NEXT')}>
-            <span className='material-icons m-0 p-0'>arrow_forward</span>
-          </button>
-        </div>
-      </div>
     </div>
-    
-   
   );
 };
