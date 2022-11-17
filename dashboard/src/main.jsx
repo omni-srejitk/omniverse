9973487982e250b8f34a55bb0467826bc9eea6ce
Sreejith K <sreejith.k@omniflo.in>
2022-11-17 14:07:43 +0530

    Feat : Redux Toolkit added

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -6,13 +6,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { FilterProvider } from './context/FilterContext/FilterContext';
 export const queryClient = new QueryClient();
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <QueryClientProvider client={queryClient}>
-        <FilterProvider>
-          <App />
-        </FilterProvider>
+        <Provider store={store}>
+          <FilterProvider>
+            <App />
+          </FilterProvider>
+        </Provider>
       </QueryClientProvider>
     </Router>
   </React.StrictMode>
