1816d9ec209b09eb6356b1b1f73d57a1e85418fd
Naresh <naresh13011999@gmail.com>
2022-11-21 16:21:44 +0530

    added Pie, vertical Bar and Composed Charts

---
+++ dashboard/src/components/Charts/BarChart/VerticalBarChart.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  CartesianGrid,
+} from 'recharts';
+export const VerticalBarChart = ({ data }) => {
+  // data = array of objects and 
+  // BarChartData for Bar chart component, XAxisValue and YAxisValue
+
+  return (
+    <ResponsiveContainer width={'100%'} height={'100%'}>
+      <BarChart 
+      width={'100%'} 
+      height={'100%'} 
+      data={data} 
+      layout="vertical"
+      >
+        <XAxis
+          dataKey={'XAxisValue'}
+          style={{
+            fontSize: '0.7rem',
+            margin: '1rem 0 0',
+            fontFamily: 'Inter',
+          }}
+          axisLine={false}
+          tickLine={false}
+          margin={{ top: -20 }}
+          interval={'preserveStartEnd'}
+          type="number"
+        />
+        <YAxis
+          dataKey={'YAxisValue'}
+          style={{
+            fontSize: '0.7rem',
+            fontFamily: 'Inter',
+          }}
+          tickLine={false}
+          axisLine={false}
+          interval={'preserveStartEnd'}
+          type="category"
+        />
+        <CartesianGrid opacity={'0.2'} />
+        <Tooltip 
+          contentStyle={{ backgroundColor: "black", opacity: '0.9', borderRadius: '1rem',}}
+          wrapperStyle={{outline: 'none'}}
+          labelStyle={{ color: "white" }}
+          itemStyle={{ color: "cyan" }}
+        />
+        <Bar dataKey={'BarChartData'} fill={'#86efac'} axisLine={false} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
---
+++ dashboard/src/components/Charts/ComposedChart/ComposedChartComp.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  ComposedChart,
+  Line,
+  Area,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+
+export const ComposedChartComp = ({ data }) => {
+ 
+  // data should have three arrays : for both charts, and XAxis 
+  // let data.barChartValue array;  barChartValue for barChart 
+  // let data.lineChartValue array; lineChart value for lineChart   
+  // let data.XAxisValue array;   
+  
+  return (
+    <ResponsiveContainer width={'100%'} height={'100%'} aspect={3}>
+      <ComposedChart
+        width="100%" 
+        height="50%" 
+        data={data}
+        >
+        <XAxis 
+        dataKey={XAxisValue} 
+        style={{
+            fontSize: '0.7rem',
+            margin: '1rem 0 0',
+            fontFamily: 'Inter',
+          }}
+        />
+        <YAxis 
+        style={{
+            fontSize: '0.7rem',
+            margin: '1rem 0 0',
+            fontFamily: 'Inter',
+          }}
+        />
+        <Tooltip 
+          contentStyle={{ backgroundColor: "black", opacity: '0.9', borderRadius: '1rem',}}
+          wrapperStyle={{outline: 'none'}}
+          labelStyle={{ color: "white" }}
+          itemStyle={{ color: "cyan" }}
+        />        
+        {/* <Area type="monotone" dataKey="" fill="#8884d8" stroke="#8884d8" /> */}
+        <Bar dataKey={barChartValue} stackOffset='sign' barSize={8} fill="#413ea0" />
+        <Line dataKey={lineChartValue} stroke="black" dot={false}/>
+        <CartesianGrid stroke="#f5f5f5" opacity='0.2'/>
+      </ComposedChart>
+    </ResponsiveContainer>
+  )
+}
