5d5a1eb61d2a75ab1b50d026532eee5935da27a4
Sreejith K <sreejith.k@omniflo.in>
2022-11-23 00:36:40 +0530

    Fix : Fixed Logo Buttons positioning

--- dashboard/src/components/Buttons/Button/Button.jsx
+++ dashboard/src/components/Buttons/Button/Button.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-export const Button = ({ children, type, active, clickFunc }) => {
+export const Button = ({
+  children,
+  absolute = false,
+  type,
+  active,
+  color,
+  clickFunc,
+  ...rest
+}) => {
   const SECONDARY = (
     <button
       onClick={clickFunc}
       className={`cursor-pointer rounded-lg px-4 py-2 font-semibold text-gray-500 hover:bg-gray-100 ${
         active ? 'bg-gray-100 text-black' : 'bg-white'
       } hover:text-black`}
+      rest
     >
       {children}
     </button>
@@ -33,9 +42,13 @@ export const Button = ({ children, type, active, clickFunc }) => {
   const LOGO_BUTTON = (
     <button
       onClick={clickFunc}
-      className='group absolute top-4 right-4 z-40 h-10 w-10 cursor-pointer rounded-lg border-2 border-gray-100 bg-white font-semibold text-gray-700 hover:border-gray-400  hover:text-black'
+      className={`group ${
+        absolute ? 'absolute' : 'flex items-center justify-center'
+      } top-4 right-4 z-40 h-10 w-10  cursor-pointer rounded-full border-2 border-gray-100 bg-white font-semibold text-gray-700 hover:border-gray-400  hover:text-black`}
     >
-      <span className='material-icons flex h-full w-full items-center justify-center rounded-lg text-gray-400 group-hover:text-black'>
+      <span
+        className={`material-icons text-base ${color} flex h-full w-full items-center justify-center rounded-lg text-gray-400 group-hover:text-black`}
+      >
         {children}
       </span>
     </button>
