951a4958cccf431fa3ab84cd8211f3fd5e8fb96b
Sreejith K <sreejith.k@omniflo.in>
2022-11-28 12:10:16 +0530

    Refactor : Moved to the new API

--- dashboard/src/routes/Dashboard/Dashboard.jsx
+++ dashboard/src/routes/Dashboard/Dashboard.jsx
@@ -1,24 +1,6 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import {
-  fetchDailyGMV,
-  fetchInventoryCount,
-  fetchStoreImages,
-} from '../../services/apiCalls';
-import { computeSalesNumber } from '../../utils/helperFunctions';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  selectAllGMVSalesData,
-  selectAllItems,
-  selectAllPrices,
-  selectAllSalesData,
-  selectAllStores,
-  selectAllFilteredDates,
-  selectAllFilteredItems,
-  selectAllFilteredStores,
-  selectLoadingState,
-  selectLoginStatus,
-} from '../../redux/actions';
-
 import {
   AreaCharts,
   BarCharts,
@@ -28,54 +10,61 @@ import {
   Select,
   Spinner,
   StatCard,
-} from '../../components';
+} from '../components';
+import {
+  selectAllStores,
+  selectCumlativeAmtData,
+  selectCumlativeCountData,
+  selectSaleAmount,
+  selectUnitsSold,
+} from '../redux/actions';
+import {
+  selectAllDates,
+  selectAllItems,
+  selectFilteredSalesData,
+} from '../redux/actions/dataActions';
 import {
   setAuthToken,
   setBrandName,
   setloginStatus,
-} from '../../redux/features/authSlice';
+} from '../redux/features/authSlice';
+import {
+  fetchDailyGMV,
+  fetchInventoryCount,
+  fetchStoreImages,
+} from '../services/apiCalls';
+import {
+  calculateDailyGMV,
+  fetchCumalativeSaleAmount,
+  fetchCumalativeSaleCount,
+  getFilteredData,
+  prepareSaleData,
+} from '../utils/helperFunctions';
 
-export const Dashboard = () => {
+export const Revamped = () => {
+  const BRAND = localStorage.getItem('Name');
   const [showState, setShowState] = useState({
     durationFilter: false,
     productFilter: false,
   });
-
-  const BRAND = localStorage.getItem('Name');
-  const { isLoading: isCalculateGMVLoading, data } = fetchDailyGMV(BRAND);
-
-  const calculateDailyGMV = (arr) => {
-    let SALE = arr?.reduce((acc, curr) => (acc += curr[2]), 0);
-    let GMV = arr?.reduce((acc, curr) => (acc += curr[7]), 0);
-  };
-
-  const filterByStore = (state, arr) => {
-    return arr?.filter((sale) => state.includes(sale[1]));
-  };
-
-  const filterByItem = (state, arr) => {
-    return arr?.filter((sale) => state.includes(sale[6]));
-  };
-
-  calculateDailyGMV(data);
-
-  const { isLoading: isInventoryLoading, data: resData } =
+  const { isLoading: isGMVLoading, data: dailyGMVData } = fetchDailyGMV(BRAND);
+  const { isLoading: isInventoryLoading, data: inventoryData } =
     fetchInventoryCount(BRAND);
+  const { isLoading: ImageLoading, data: imageData } = fetchStoreImages(BRAND);
   const dispatch = useDispatch();
-  const isGMVDataLoading = useSelector(selectLoadingState);
-  const PRICES = useSelector(selectAllPrices);
-  const SALE_DATA = useSelector(selectAllSalesData);
-  const ISLOGGED = useSelector(selectLoginStatus);
+  const UNITS_SOLD = useSelector(selectUnitsSold);
+  const SALE_AMT = useSelector(selectSaleAmount);
+  const LIVESTORES = useSelector(selectAllStores);
+  const LIVESTORES_COUNT = useSelector(selectAllStores)?.length;
+  const BARCHART = useSelector(selectCumlativeCountData);
+  const AREACHART = useSelector(selectCumlativeAmtData);
+  const FILTERSTATE = useSelector((state) => state.filter);
+  const FILTEREDSALEDATA = useSelector(selectFilteredSalesData);
   const ALLITEMS = useSelector(selectAllItems);
-  const ALLSTORES = useSelector(selectAllStores);
-  const FILTERED_DATES = useSelector(selectAllFilteredDates);
-  const FILTERED_ITEMS = useSelector(selectAllFilteredItems);
-  const FILTERED_STORES = useSelector(selectAllFilteredStores);
-
-  const LIVESTORES_LENGTH = ALLSTORES?.length;
+  const ALLDATES = useSelector(selectAllDates);
   const TOKEN = JSON.parse(localStorage.getItem('Token'));
   const NAME = localStorage.getItem('Name');
-  const GMV_SALES_DATA = useSelector(selectAllGMVSalesData);
+
   useEffect(() => {
     if (TOKEN) {
       dispatch(setloginStatus(true));
@@ -84,39 +73,24 @@ export const Dashboard = () => {
     }
   }, [TOKEN, NAME]);
 
-  const { cumlativeSalesReport } = useMemo(() => {
-    const STORES =
-      FILTERED_STORES?.length === 0
-        ? ALLSTORES.map((store) => store.customer_name)
-        : FILTERED_STORES;
-    const ITEMS = FILTERED_ITEMS?.length === 0 ? ALLITEMS : FILTERED_ITEMS;
-    return computeSalesNumber(
-      FILTERED_DATES,
-      STORES,
-      ITEMS,
-      PRICES,
-      GMV_SALES_DATA,
-      SALE_DATA
-    );
-  }, [
-    FILTERED_DATES,
-    FILTERED_STORES,
-    FILTERED_ITEMS,
-    PRICES,
-    GMV_SALES_DATA,
-    SALE_DATA,
-  ]);
+  useEffect(() => {
+    if (!isGMVLoading && FILTEREDSALEDATA) {
+      calculateDailyGMV(FILTEREDSALEDATA, dispatch);
+      fetchCumalativeSaleCount(FILTEREDSALEDATA, ALLDATES, dispatch);
+      fetchCumalativeSaleAmount(FILTEREDSALEDATA, ALLDATES, dispatch);
+    }
+  }, [FILTEREDSALEDATA]);
 
-  const InventoryData = !isInventoryLoading && resData?.data['message'];
+  useEffect(() => {
+    if (!isGMVLoading) {
+      prepareSaleData(dailyGMVData, dispatch);
+    }
+  }, [isGMVLoading, dailyGMVData]);
 
   const DASHBOARD_FILTERS = {
     'By Product': ALLITEMS,
-    'By Store': ALLSTORES.map((store) => store.customer_name),
+    'By Store': LIVESTORES,
   };
-
-  const { isLoading: ImageLoading, data: ImageData } = fetchStoreImages(BRAND);
-
-  let IMG_DATA = !ImageLoading && ImageData?.data['message'];
 
   const OVERVIEW_FILTERS = (
     <div className='items-centerS flex gap-4'>
@@ -129,107 +103,115 @@ export const Dashboard = () => {
     </div>
   );
 
-  return (
-    ISLOGGED && (
-      <main className='page__content'>
-        <section className='h-fit w-full'>
-          <h1 className='page__title'>Welcome {BRAND}! 👋</h1>
-          <Card title='Overview' cardHeader={OVERVIEW_FILTERS}>
-            <div className='card_body flex h-fit w-full justify-start overflow-x-auto scrollbar-thin'>
-              <StatCard
-                icon='home'
-                title='Units Sold'
-                metric={cumlativeSalesReport.TOTAL_SALES}
-                loading={isGMVDataLoading}
-                background='bg-green-100'
-                spinner={'border-green-200'}
-                tooltip={'Total count of items sold.'}
-              />
-              <StatCard
-                icon='insights'
-                title='Total GMV'
-                metric={cumlativeSalesReport.TOTAL_GMV}
-                loading={isGMVDataLoading}
-                tooltip={'Total sale of items sold.'}
-                background='bg-blue-100'
-                spinner={'border-blue-200'}
-                currency
-              />
-              <StatCard
-                icon='store'
-                title='Total Stores'
-                loading={isGMVDataLoading}
-                metric={LIVESTORES_LENGTH}
-                background='bg-purple-100'
-                spinner={'border-purple-200'}
-                tooltip={'Total no of stores where item is active.'}
-              />
-              <StatCard
-                icon='store'
-                title='Total Deployed'
-                metric={InventoryData}
-                loading={isInventoryLoading}
-                tooltip={'Total no of items in inventory deployed.'}
-                background='bg-yellow-100'
-                spinner={'border-yellow-200'}
-              />
-            </div>
-          </Card>
-        </section>
-        <section className='grid-rows-[repeat(2,20rem] my-4 mb-36 grid h-full w-full grid-cols-1 gap-4 md:mb-0 lg:grid-cols-3 lg:pb-6'>
-          <Card
-            title='Cumalative Sales'
-            classes={
-              'row-span-1 lg:col-span-2 order-1 max-h-[20rem] flex-grow overflow-hidden justify-center items-center'
-            }
-          >
-            <div className='relative flex h-full w-full flex-grow items-center justify-center rounded-xl bg-gray-100/50'>
-              {isGMVDataLoading ? (
-                <Spinner
-                  color={'border-blue-200'}
-                  position={'top-1/2 left-1/2'}
-                  loading={isGMVDataLoading}
-                />
-              ) : (
-                <AreaCharts
-                  data={cumlativeSalesReport.GRAPH_DATA}
-                  color={'#3b82f6'}
-                />
-              )}
-            </div>
-          </Card>
-          <Card
-            title='Showcase'
-            classes={
-              'row-span-2 flex-grow max-h-[42rem] col-span-1 order-3 lg:order-2 '
-            }
-          >
-            <Carousal src={IMG_DATA} loading={ImageLoading} />
-          </Card>
+  useEffect(() => {
+    if (isGMVLoading) return;
+    getFilteredData(FILTERSTATE, dailyGMVData, dispatch);
+  }, [isGMVLoading, FILTERSTATE, dailyGMVData]);
 
-          <Card
-            title='Sales '
-            classes={
-              'row-span-1 lg:col-span-2 max-h-[20rem] flex-grow overflow-hidden justify-center items-center order-2'
-            }
-          >
-            <div className='relative flex h-full w-full flex-grow items-center justify-center rounded-xl  bg-gray-100/50'>
-              {isGMVDataLoading ? (
-                <Spinner
-                  color={'border-green-200'}
-                  position={'top-1/2 left-1/2'}
-                  loading={isGMVDataLoading}
-                />
-              ) : (
-                <BarCharts
-                  data={cumlativeSalesReport.GRAPH_DATA}
-                  color={'#86efac'}
-                />
-              )}
-            </div>
-          </Card>
-        </section>
-      </main>
-    )
+  return (
+    <main className='page__content'>
+      <section className='h-fit w-full'>
+        <h1 className='page__title'>Welcome {BRAND}! 👋</h1>
+        <Card title='Overview' cardHeader={OVERVIEW_FILTERS}>
+          <div className='card_body flex h-fit w-full justify-start overflow-x-auto scrollbar-thin'>
+            <StatCard
+              icon='home'
+              title='Units Sold'
+              metric={UNITS_SOLD}
+              loading={isGMVLoading}
+              background='bg-green-100'
+              spinner={'border-green-200'}
+              tooltip={'Total count of items sold.'}
+            />
+            <StatCard
+              icon='insights'
+              title='Total GMV'
+              metric={SALE_AMT}
+              loading={isGMVLoading}
+              tooltip={'Total sale of items sold.'}
+              background='bg-blue-100'
+              spinner={'border-blue-200'}
+              currency
+            />
+            <StatCard
+              icon='store'
+              title='Total Stores'
+              loading={isGMVLoading}
+              metric={LIVESTORES_COUNT}
+              background='bg-purple-100'
+              spinner={'border-purple-200'}
+              tooltip={'Total no of stores where item is active.'}
+            />
+            <StatCard
+              icon='warehouse'
+              title='Total Deployed'
+              metric={inventoryData}
+              loading={isInventoryLoading}
+              tooltip={'Total no of items in inventory deployed.'}
+              background='bg-yellow-100'
+              spinner={'border-yellow-200'}
+            />
+          </div>
+        </Card>
+      </section>
+      <section className='grid-rows-[repeat(2,20rem] my-4 mb-36 grid h-full w-full grid-cols-1 gap-4 md:mb-0 lg:grid-cols-3 lg:pb-6'>
+        <Card
+          title='Cumalative Sales'
+          classes={
+            'row-span-1 lg:col-span-2 order-1 max-h-[20rem] flex-grow overflow-hidden justify-center items-center'
+          }
+        >
+          <div className='relative flex h-full w-full flex-grow items-center justify-center rounded-xl bg-gray-100/50'>
+            {isGMVLoading ? (
+              <Spinner
+                color={'border-blue-200'}
+                position={'top-1/2 left-1/2'}
+                loading={isGMVLoading}
+              />
+            ) : (
+              <AreaCharts
+                data={AREACHART}
+                XAxisKey={'Date'}
+                YAxisKey={'Unit_Amt'}
+                DataKey={'Unit_Amt'}
+                color={'#3b82f6'}
+              />
+            )}
+          </div>
+        </Card>
+        <Card
+          title='Showcase'
+          classes={
+            'row-span-2 flex-grow max-h-[42rem] col-span-1 order-3 lg:order-2 '
+          }
+        >
+          <Carousal src={imageData} loading={ImageLoading} />
+        </Card>
+        <Card
+          title='Sales '
+          classes={
+            'row-span-1 lg:col-span-2 max-h-[20rem] flex-grow overflow-hidden justify-center items-center order-2'
+          }
+        >
+          <div className='relative flex h-full w-full flex-grow items-center justify-center rounded-xl  bg-gray-100/50'>
+            {isGMVLoading ? (
+              <Spinner
+                color={'border-green-200'}
+                position={'top-1/2 left-1/2'}
+                loading={isGMVLoading}
+              />
+            ) : (
+              <BarCharts
+                data={BARCHART}
+                XAxisKey={'Date'}
+                YAxisKey={'Unit_Sold'}
+                DataKey={'Unit_Sold'}
+                color={'#86efac'}
+              />
+            )}
+          </div>
+        </Card>
+      </section>
+    </main>
   );
 };
