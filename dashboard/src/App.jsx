943ecb2b66ddb1c2406d4571bf893b8650dc81c2
Sreejith K <sreejith.k@omniflo.in>
2022-11-29 09:57:27 +0530

    Feat : Added Tab titles

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -4,15 +4,21 @@ import { Sidebar } from './components/Sidebar/Sidebar';
 import { RouterConfig } from './config/RouterConfig';
 import { Toaster } from 'react-hot-toast';
 import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
   const { pathname } = useLocation();
+  const PAGETITLE =
+    pathname.slice(1).slice(0, 1).toUpperCase() + pathname.slice(1).slice(1);
+
+  useEffect(() => {
+    document.title = PAGETITLE + ' | Omniverse';
+  }, [pathname]);
 
   return (
     <div className='App'>
       {pathname == '/' ? null : <Sidebar />}
       {pathname == '/' ? null : <Header />}
-
       <RouterConfig />
       <Toaster />
     </div>
