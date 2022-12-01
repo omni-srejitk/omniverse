6504dcbc3d4d53c0b41d4b24b7fc48846185d884
Sreejith K <sreejith.k@omniflo.in>
2022-12-01 15:28:16 +0530

    Feat : Added Custom Legend/Tooltip

--- dashboard/src/components/Charts/PieChart/PieChartComp.jsx
+++ dashboard/src/components/Charts/PieChart/PieChartComp.jsx
@@ -1,48 +1,100 @@
-import React from 'react'
-import { 
+import React from 'react';
+import {
   ResponsiveContainer,
   PieChart,
   Pie,
   Cell,
-  Tooltip } from 'recharts';
+  Tooltip,
+  Legend,
+} from 'recharts';
 
-export const PieChartComp = ({colors}, data) => {
-
-  // Data format for pie chart => an array of objects with name and value keys and their respective values and pass colors array with colors that you want to show there 
+export const PieChartComp = ({ data = {} }) => {
+  // Data format for pie chart => an array of objects with name and value keys and their respective values and pass colors array with colors that you want to show there
   // const data = [{  "name": "Group A", "value": 400  }]
-  // colors array like this => const COLORS = ['#0088FE', '#FFBB28', '#41e414','#d44133','#00C49F','#FF8042']; 
+  // colors array like this =>
+  const colors = [
+    '#0088FE',
+    '#FFBB28',
+    '#41e414',
+    '#d44133',
+    '#00C49F',
+    '#FF8042',
+  ];
+
+  const CustomTooltip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className='flex flex-col items-start rounded-lg border-2 border-gray-200 bg-white/90 p-4'>
+          <p className='font-thin'>
+            <span className=' font-medium text-black'>Range:</span>
+            {`${payload[0].name}`}
+          </p>
+          <p className='font-thin'>
+            <span className=' font-medium text-black'>Count:</span>
+            {`${payload[0].value}`}
+          </p>
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  const CustomLegend = ({ payload }) => {
+    console.log(payload);
+    if (payload && payload.length) {
+      return (
+        <div className='mt-8 flex w-full justify-between'>
+          {payload?.map(({ payload }) => (
+            <div
+              key={payload.cell}
+              className='flex h-24 w-40 flex-col items-center rounded-lg bg-white/90'
+            >
+              <div
+                style={{ background: payload.fill }}
+                className={`mb-2 h-4 w-4 rounded-lg`}
+              ></div>
+
+              <p
+                style={{ color: payload.fill }}
+                className='mb-2 font-semibold text-gray-400'
+              >{`${payload.name} `}</p>
+              <p className='mb-2 text-xl font-semibold text-gray-700'>{`${(
+                payload.percent * 100
+              ).toFixed(1)}%`}</p>
+            </div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
   return (
-    <ResponsiveContainer 
-    width={'100%'} 
-    height={'100%'}
-    margin={{ top: 0 }}
-    >
-      <PieChart 
-      width={'100%'} 
-      height={'100%'}
-      >
-        <Pie 
-        data={data} 
-        dataKey={data.value}
-        nameKey="name" 
-        cx="50%" 
-        cy="50%" 
-        innerRadius={70}
-        outerRadius={95} 
-        fill="#8884d8" 
-        labelLine={false}
+    <ResponsiveContainer width={'100%'} height={'100%'} margin={{ top: 0 }}>
+      <PieChart width={'100%'} height={'100%'}>
+        <Pie
+          data={data}
+          dataKey={data.value}
+          nameKey='name'
+          cx='50%'
+          cy='50%'
+          innerRadius={70}
+          outerRadius={95}
+          fill='#8884d8'
+          labelLine={false}
         >
           {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+            <Cell
+              dataKey={`cell-${index}`}
+              fill={colors[index % colors.length]}
+            />
           ))}
         </Pie>
-        <Tooltip 
-          contentStyle={{ backgroundColor: "black", opacity: '0.9', borderRadius: '1rem',}}
-          wrapperStyle={{outline: 'none'}}
-          labelStyle={{ color: "white" }}
-          itemStyle={{ color: "white" }}
-        />
+
+        <Tooltip content={<CustomTooltip />} />
+        <Legend content={<CustomLegend />} />
       </PieChart>
-    </ResponsiveContainer>    
-  )
-}
+    </ResponsiveContainer>
+  );
+};
