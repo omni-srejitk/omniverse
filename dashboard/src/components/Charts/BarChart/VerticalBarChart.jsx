3a4c4f1916ba691cc6e39a0b846d61ff08f49fec
Sreejith K <sreejith.k@omniflo.in>
2022-12-07 18:09:21 +0530

    Feat : Added Vertical Barcharts for Analytics

--- dashboard/src/components/Charts/BarChart/VerticalBarChart.jsx
+++ dashboard/src/components/Charts/BarChart/VerticalBarChart.jsx
@@ -8,20 +8,21 @@ import {
   ResponsiveContainer,
   CartesianGrid,
 } from 'recharts';
-export const VerticalBarChart = (data) => {
-  // data = array of objects and 
-  // BarChartData for Bar chart component
+export const VerticalBarChart = (props) => {
+  const { data, dataKey, color = '#86efac', XAxisKey, YAxisKey } = props;
 
+  const SLICE_LENGTH = data?.length > 5 ? 5 : data?.length;
+  const SLICED_DATA = data?.slice(0, SLICE_LENGTH);
   return (
     <ResponsiveContainer width={'100%'} height={'100%'}>
-      <BarChart 
-      width={'100%'} 
-      height={'100%'} 
-      data={data} 
-      layout="vertical"
+      <BarChart
+        width={'100%'}
+        height={'100%'}
+        data={SLICED_DATA}
+        layout='vertical'
       >
         <XAxis
-          dataKey={data.XAxisValue}
+          dataKey={XAxisKey}
           style={{
             fontSize: '0.7rem',
             margin: '1rem 0 0',
@@ -31,27 +32,28 @@ export const VerticalBarChart = (data) => {
           tickLine={false}
           margin={{ top: -20 }}
           interval={'preserveStartEnd'}
-          type="number"
+          type='number'
         />
         <YAxis
-          dataKey={data.YAxisValue}
-          style={{
-            fontSize: '0.7rem',
-            fontFamily: 'Inter',
-          }}
+          dataKey={YAxisKey}
           tickLine={false}
           axisLine={false}
+          tickMargin={5}
           interval={'preserveStartEnd'}
-          type="category"
+          type='category'
         />
         <CartesianGrid opacity={'0.2'} />
-        <Tooltip 
-          contentStyle={{ backgroundColor: "black", opacity: '0.9', borderRadius: '1rem',}}
-          wrapperStyle={{outline: 'none'}}
-          labelStyle={{ color: "white" }}
-          itemStyle={{ color: "cyan" }}
+        <Tooltip
+          contentStyle={{
+            backgroundColor: 'black',
+            opacity: '0.9',
+            borderRadius: '1rem',
+          }}
+          wrapperStyle={{ outline: 'none' }}
+          labelStyle={{ color: 'white' }}
+          itemStyle={{ color: 'cyan' }}
         />
-        <Bar dataKey={data.XAxisValue} fill={'#86efac'} axisLine={false} />
+        <Bar dataKey={dataKey} fill={color} axisLine={false} />
       </BarChart>
     </ResponsiveContainer>
   );
