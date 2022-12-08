765e994a45e2ccdbadc849dd68dbd5e6330c3e3e
Sreejith K <sreejith.k@omniflo.in>
2022-12-08 18:59:28 +0530

    Fix : Total Sales Count

--- dashboard/src/components/Cards/AnalyticsLineGraphCard/AnalyticsChart.jsx
+++ dashboard/src/components/Cards/AnalyticsLineGraphCard/AnalyticsChart.jsx
@@ -5,7 +5,6 @@ import {
   selectAllFilteredDates,
   selectCumlativeAmtData,
   selectCumlativeCountData,
-  selectSaleAmount,
 } from '../../../redux/actions';
 import { computeAnalyticsSalesNumber2 } from '../../../utils/helperFunctions';
 import { LineChartComponent } from '../../Charts/LineChart/LineChartComponent';
@@ -13,7 +12,6 @@ import { LineChartComponent } from '../../Charts/LineChart/LineChartComponent';
 export const AnalyticsChart = ({ data }) => {
   const FILTEREDDATE = useSelector(selectAllFilteredDates);
   const FIRSTDATE = moment(FILTEREDDATE, 'DD-MM-YY').format('LL');
-  const SALE_AMOUNT = useSelector(selectSaleAmount);
   const LASTDATE = moment().format('LL');
   const DIFF = moment(LASTDATE, 'LL').diff(moment(FIRSTDATE, 'LL'), 'days');
   const CUMLATIVE_SALE = useSelector(selectCumlativeCountData);
@@ -24,19 +22,13 @@ export const AnalyticsChart = ({ data }) => {
     DIFF
   );
 
-  const PERC_SALE = PREV_DATA?.percentageChangeGMV;
+  const TOTAL_SALES =
+    data.length === 0 ? 0 : data?.reduce((acc, curr) => (acc += curr[7]), 0);
+
   return (
     <div className='h-full max-h-[18rem] w-full'>
-      <h1 className='text-3xl font-semibold'>&#8377;{SALE_AMOUNT}</h1>
-      <div className='my-2 mb-4  flex items-center gap-2'>
-        <div className='flex w-fit items-center justify-center gap-1 rounded-xl bg-green-200 p-1'>
-          <span className='material-icons m-0 p-0 text-sm'>
-            {PERC_SALE > 0 ? 'arrow_upwards' : 'arrow_downwards'}
-          </span>
-          <p className='pr-2 text-sm font-medium'>{PERC_SALE || 0}%</p>
-        </div>
-        <p className='font-medium text-gray-400'>vs {FIRSTDATE}</p>
-      </div>
+      <h1 className='mb-6 text-3xl font-semibold'>&#8377;{TOTAL_SALES}</h1>
+
       <LineChartComponent data={data} />
     </div>
   );
