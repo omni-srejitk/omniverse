0db299678530363d66a210375e1e427218b5cd3c
Sreejith K <sreejith.k@omniflo.in>
2022-12-08 19:00:05 +0530

    Refactor : Cleaned up code

--- dashboard/src/components/Charts/GenderChart/GenderChart.jsx
+++ dashboard/src/components/Charts/GenderChart/GenderChart.jsx
@@ -5,24 +5,30 @@ export const GenderChart = ({ data = {} }) => {
   const MALE_PERC = Math.round((+data.Male * 100) / +data.All);
   const FEMALE_PERC = Math.round((+data.Female * 100) / +data.All);
 
+  const GENDER_RATIO_CHART = ({ type }) => {
+    return (
+      <div className=' flex h-full flex-grow flex-col items-center justify-center gap-4'>
+        <div className=' flex w-full items-center'>
+          <span
+            className={`material-icons flex w-full items-center justify-center text-9xl ${
+              type === 'MALE' ? 'text-green-400/70' : 'text-blue-400/70'
+            }`}
+          >
+            {type === 'MALE' ? 'male' : 'female'}
+          </span>
+        </div>
+        <ProgressBar
+          label={type === 'MALE' ? 'Male' : 'Female'}
+          value={type === 'MALE' ? MALE_PERC : FEMALE_PERC}
+        />
+      </div>
+    );
+  };
+
   return (
     <div className='flex h-full w-full items-center justify-between gap-8'>
-      <div className=' flex h-full flex-grow flex-col items-start justify-start'>
-        <div className=' flex h-full w-full items-center'>
-          <span className='material-icons flex w-full items-center justify-center text-7xl text-green-400'>
-            man
-          </span>
-        </div>
-        <ProgressBar label='Male' value={MALE_PERC} />
-      </div>
-      <div className=' flex h-full flex-grow flex-col items-start justify-start'>
-        <div className=' flex h-full w-full items-center'>
-          <span className='material-icons flex w-full items-center justify-center text-7xl text-red-400'>
-            woman
-          </span>
-        </div>
-        <ProgressBar label='Female' value={FEMALE_PERC} />
-      </div>
+      <GENDER_RATIO_CHART type='MALE' />
+      <GENDER_RATIO_CHART type='FEMALE' />
     </div>
   );
 };
