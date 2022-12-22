ef17c5e4a61953eddf0b513eca9a7040633114b2
Sreejith K <sreejith.k@omniflo.in>
2022-12-22 19:48:58 +0530

    Fix : Inconsistent Sales/Count

--- dashboard/src/components/Select/Select.jsx
+++ dashboard/src/components/Select/Select.jsx
@@ -1,48 +1,47 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { DATE_FILTERS } from '../../utils/constants';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { DATE_FILTERS } from "../../utils/constants";
 import {
   selectAllDates,
   selectDurationFilter,
   selectPopupState,
-} from '../../redux/actions';
+} from "../../redux/actions";
 
 import {
   setDurationFilter,
   setFilterEndDate,
   setFilterStartDate,
-} from '../../redux/features/filterSlice';
+} from "../../redux/features/filterSlice";
 import {
   setDatePickerPopup,
   setDurationPopup,
-} from '../../redux/features/popupSlice';
+} from "../../redux/features/popupSlice";
 
 export const Select = () => {
   const dispatch = useDispatch();
   const FILTERBYDURATION = useSelector(selectDurationFilter);
   const SHOWPOPUP = useSelector(selectPopupState);
   const SALEDATE = useSelector(selectAllDates);
-  const FIRSTSALEDATE = SALEDATE.slice(0,1)[0]
-
+  const FIRSTSALEDATE = SALEDATE.slice(0, 1)[0];
 
   return (
-    <div className='relative w-max '>
+    <div className="relative w-max ">
       <div
         onClick={() =>
           SHOWPOPUP.durationFilter
             ? dispatch(setDurationPopup(false))
             : dispatch(setDurationPopup(true))
         }
-        className='flex cursor-pointer items-center justify-between rounded-lg border-2  border-gray-100 bg-white px-4 py-2 font-medium hover:border-gray-400'
+        className="flex cursor-pointer items-center justify-between rounded-lg border-2  border-gray-100 bg-white px-4 py-2 font-medium hover:border-gray-400"
       >
         {FILTERBYDURATION}
-        <span className='material-icons'>
-          {SHOWPOPUP.durationFilter ? 'expand_less' : 'expand_more'}
+        <span className="material-icons">
+          {SHOWPOPUP.durationFilter ? "expand_less" : "expand_more"}
         </span>
       </div>
       <ul
         className={` ${
-          !SHOWPOPUP.durationFilter ? 'invisible h-0' : 'h-fit'
+          !SHOWPOPUP.durationFilter ? "invisible h-0" : "h-fit"
         } absolute right-0 z-20 my-1 flex w-40 flex-col items-center justify-start rounded-lg border-2 border-gray-50 bg-white `}
       >
         {DATE_FILTERS?.map((item) => (
@@ -50,18 +49,20 @@ export const Select = () => {
             key={item.id}
             onClick={(e) => {
               dispatch(setDurationFilter(item.title));
-              if (item.title === 'Custom Range') {
+              if (item.title === "Custom Range") {
                 dispatch(setDatePickerPopup(true));
-              } else if (item.title === 'Lifetime') {
-                dispatch(setFilterStartDate(FIRSTSALEDATE));
+              } else if (item.title === "Lifetime") {
+                //dispatch(setFilterStartDate(FIRSTSALEDATE));
+                dispatch(setFilterStartDate(item.startDate));
                 dispatch(setFilterEndDate(item.endDate));
+                dispatch(setDurationPopup(false));
               } else {
                 dispatch(setFilterStartDate(item.startDate));
                 dispatch(setFilterEndDate(item.endDate));
                 dispatch(setDurationPopup(false));
               }
             }}
-            className=' w-full cursor-pointer px-4 py-2 text-right font-semibold text-gray-500 hover:bg-gray-100 hover:text-black'
+            className=" w-full cursor-pointer px-4 py-2 text-right font-semibold text-gray-500 hover:bg-gray-100 hover:text-black"
           >
             {item.title}
           </li>
